71c43aaf945ca3d0bf8acb6a6aa18aa4638b8024
Jestina Bangura <BanguraJestina@gmail.com>
2024-02-19 22:30:57 +0000

    bit-operators

---
+++ Bit-Operators.py
@@ -0,0 +1,18 @@
+first_bit = 1
+second_bit = 1
+
+print(first_bit & second_bit)
+
+first_bit = 0
+second_bit = 0
+
+print(first_bit | second_bit)
+
+first_bit = 0
+second_bit = 0
+
+print(first_bit ^ second_bit)
+
+print(~0)
+
+12 << 1
