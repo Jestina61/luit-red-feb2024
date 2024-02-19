f46bf208914d04e3de72860020363966cd21f149
Jestina Bangura <BanguraJestina@gmail.com>
2024-02-19 23:40:17 +0000

    Joining multiple-conditions

---
+++ module3-joining-multiple-conditions.py
@@ -0,0 +1,16 @@
+user_age = int(input('What is your age? '))
+user_country = input('What is your country? ')
+
+if user_age < 25 and user_country == 'Germany':
+    print('You can apply for a German student exchange programme')
+else:
+    print('SOrry, you do not qualify')
+    
+user_age =int(input('What is your agr? '))
+user_country = input('What is your country? ')
+
+if not user_country == 'Germany' and user_age < 25 or \
+    user_country == 'Germany' and user_age < 23:
+    print('You qualify!')
+else:
+    print('You don\'t qualify!')
